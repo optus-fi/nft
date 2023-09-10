b88d6c79a640a14a76588e95a13469fb0019d494
mitagmio <mitag.mio@gmail.com>
2023-09-10 17:38:08 +0000

    fix contract wrapper

--- wrappers/NftCollection.ts
+++ wrappers/NftCollection.ts
@@ -130,7 +130,7 @@ export class NftCollection implements Contract {
         await provider.internal(via, {
             value: toNano('0.1') * BigInt(dict.size) + toNano('0.05'),
             sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell().storeUint(2, 32).storeUint(0, 64).storeDict(dict).endCell(),
+            body: beginCell().storeUint(2, 32).storeUint(0, 64).storeDict(dict).storeUint(1, 2).endCell(),
         });
     }
 
