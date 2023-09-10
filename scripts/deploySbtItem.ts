81bcd3931ec314c038a190f6b02368e47f7837da
mitagmio <mitag.mio@gmail.com>
2023-09-10 13:04:28 +0000

    fix

--- scripts/deploySbtItem.ts
+++ scripts/deploySbtItem.ts
@@ -3,22 +3,20 @@ import { compile, NetworkProvider } from '@ton-community/blueprint';
 import { NftCollection } from '../wrappers/NftCollection';
 
 export async function run(provider: NetworkProvider) {
-
     const ui = provider.ui();
 
     const nftCollection = provider.open(
         NftCollection.createFromAddress(Address.parse('EQCvvleli-ANUGQyW8HwDScoogBjq3P7xNCpNGg5ks5uQJ4W'))
     );
 
-    await nftCollection.sendMint(provider.sender(), 
-        {
-            amount: toNano('0.1'),
-            itemOwnerAddress: provider.sender().address as Address,
-            itemAuthorityAddress: provider.sender().address as Address,
-            itemContent: 'https://raw.githubusercontent.com/liminalAngel/nft2/main/scripts/item.json',
-            itemIndex: 0,
-        }
-    )
+    await nftCollection.sendMint(provider.sender(), {
+        amount: toNano('0.1'),
+        itemOwnerAddress: provider.sender().address as Address,
+        itemAuthorityAddress: provider.sender().address as Address,
+        itemContent:
+            'https://raw.githubusercontent.com/optus-fi/nft/master/scripts/sbt-bug-collection/items/json/0.json',
+        itemIndex: 0,
+    });
 
-    ui.write("SBT deployed!");
+    ui.write('SBT deployed!');
 }
