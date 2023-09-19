5320984cb5e07adbd18d6d6e2aaf7218e9685a85
mitagmio <mitag.mio@gmail.com>
2023-09-19 15:38:04 +0000

    add SBT CONTRIB

---
+++ scripts/deployBatchSbtItemsContrib.ts
@@ -0,0 +1,50 @@
+import { Address, toNano } from 'ton-core';
+import { compile, NetworkProvider } from '@ton-community/blueprint';
+import { NftCollection } from '../wrappers/NftCollection';
+
+export async function run(provider: NetworkProvider) {
+    const ui = provider.ui();
+
+    const nftCollection = provider.open(
+        NftCollection.createFromAddress(Address.parse('EQBwTO1wu1rTpzlN1EVtPuPpJKrCLXR9XGe5q3tvD9A22x0v'))
+    );
+
+    await nftCollection.sendBatchMint(provider.sender(), {
+        sbts: [
+            {
+                amount: toNano('0.1'),
+                ownerAddress: Address.parse('EQDmYoHvv2ZSnpjMlH1LDsJorTvtEF_sRN2hoDlHMeoCXO4I'),
+                authorityAddress: provider.sender().address as Address,
+                content:
+                    'https://raw.githubusercontent.com/optus-fi/nft/master/scripts/sbt-bug-collection/items/json/0.json',
+                index: 0,
+            },
+            {
+                amount: toNano('0.1'),
+                ownerAddress: Address.parse('EQB9tsqI3aTrUyOSnvsae-NB4S1ANxBXi95NyZ7hVjewTDfN'),
+                authorityAddress: provider.sender().address as Address,
+                content:
+                    'https://raw.githubusercontent.com/optus-fi/nft/master/scripts/sbt-bug-collection/items/json/1.json',
+                index: 1,
+            },
+            {
+                amount: toNano('0.1'),
+                ownerAddress: Address.parse('EQAl62-Cs-sdwC8wh4PHpsPZf_ojHP8ptlPuMv-lS9FkLO7b'),
+                authorityAddress: provider.sender().address as Address,
+                content:
+                    'https://raw.githubusercontent.com/optus-fi/nft/master/scripts/sbt-bug-collection/items/json/2.json',
+                index: 2,
+            },
+            {
+                amount: toNano('0.1'),
+                ownerAddress: Address.parse('EQAlOqSXNBfYa0B9qq5gKWphiDTYH62abkDskNdpm6VXwPjU'),
+                authorityAddress: provider.sender().address as Address,
+                content:
+                    'https://raw.githubusercontent.com/optus-fi/nft/master/scripts/sbt-bug-collection/items/json/3.json',
+                index: 3,
+            },
+        ],
+    });
+
+    ui.write('SBTs deployed!');
+}
---
+++ scripts/deployNftCollectionBug.ts
@@ -0,0 +1,34 @@
+import { Address, toNano } from 'ton-core';
+import { NftCollection } from '../wrappers/NftCollection';
+import { compile, NetworkProvider } from '@ton-community/blueprint';
+
+export async function run(provider: NetworkProvider) {
+    const nftCollection = provider.open(
+        NftCollection.createFromConfig(
+            {
+                ownerAddress: provider.sender().address as Address,
+                nextItemIndex: 0,
+                collectionContent:
+                    'https://raw.githubusercontent.com/optus-fi/nft/master/scripts/sbt-bug-collection/collection_mdata.json',
+                commonContent: '',
+                nftItemCode: await compile('SbtItem'),
+                royaltyParams: {
+                    royaltyFactor: 5,
+                    royaltyBase: 100,
+                    royaltyAddress: provider.sender().address as Address,
+                },
+            },
+            await compile('NftCollection')
+        )
+    );
+    // const nftCollection = provider.open(
+    //     NftCollection.createFromAddress(Address.parse('EQCxxQjTWmvFYceOyRxs1DMTcRC1Zd8K9GT-jsf3DaOSn-WI'))
+    // );
+    await nftCollection.sendDeploy(provider.sender(), toNano('0.05'));
+
+    await provider.waitForDeploy(nftCollection.address);
+
+    // run methods on `nftCollection`
+    //'b5ee9c7201010101000300000201';
+    // await nftCollection.getCollectionData();
+}
