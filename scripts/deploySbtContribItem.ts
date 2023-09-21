f977e04ea55af5929de50c1af9afc92ac03ec45b
mitagmio <mitag.mio@gmail.com>
2023-09-21 08:07:06 +0000

    fix addr

--- scripts/deploySbtContribItem.ts
+++ scripts/deploySbtContribItem.ts
@@ -6,12 +6,12 @@ export async function run(provider: NetworkProvider) {
     const ui = provider.ui();
 
     const nftCollection = provider.open(
-        NftCollection.createFromAddress(Address.parse('EQCv4bQyZQ5Rh4xa8MmvX3TZTDqoWQR_ec15BWc43kZzeLZk'))
+        NftCollection.createFromAddress(Address.parse('EQCD8AJyKeCAEtzvYYXo3-yXw19sCw70LuVjJCanF_YbwoVQ'))
     );
 
     await nftCollection.sendMint(provider.sender(), {
         amount: toNano('0.1'),
-        itemOwnerAddress: Address.parse('EQDNbXPLabqHcfpRMAQve6oRnJ4fXdyra6kWm2ZLJGKS3MWi'),
+        itemOwnerAddress: Address.parse('EQCv4bQyZQ5Rh4xa8MmvX3TZTDqoWQR_ec15BWc43kZzeLZk'),
         itemAuthorityAddress: provider.sender().address as Address,
         itemContent:
             'https://raw.githubusercontent.com/optus-fi/nft/master/scripts/sbt-bug-collection/items/json/4.json',
